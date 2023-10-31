83a1cd73b8b7fb06fd774663de9ed62d738fef09
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-10-31 17:29:27 -0400

    added functionality to my button

--- app/src/components/StartButton.jsx
+++ app/src/components/StartButton.jsx
@@ -1,10 +1,18 @@
 import { Button } from "@mantine/core";
+import { useState } from "react";
 
 const StartButton = () => {
+  const [start, setStart] = useState(false);
+
+  const startButtonClickHandler = () => {
+    setStart();
+    console.log("i have been awaken!")
+  };
+
   return (
     <div>
       <p>Match all the terms with their definitions as fast as you can!</p>
-      <Button variant="filled">Start</Button>
+      <Button variant="filled" onClick={startButtonClickHandler}>Start</Button>
     </div>
   );
 };
