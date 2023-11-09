0c3f5aeb8e3e2f479e5c765d3ce7382b8a41e44b
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-11-09 17:07:47 -0500

    trimmed excess definitions for noun definition key, changed word array to object array

--- app/src/components/CardMatches.jsx
+++ app/src/components/CardMatches.jsx
@@ -3,6 +3,7 @@ import { SimpleGrid } from "@mantine/core";
 
 function CardMatches() {
   const [data, setData] = useState([]);
+  const [nounDefinition, setNounDefinition] = useState("");
 
   useEffect(() => {
 
@@ -15,9 +16,16 @@ function CardMatches() {
           throw new Error(`Request failed with status: ${response.status}`);
         }
 
+        //Get response from API and set the state
         const worddata = await response.json();
         setData(worddata)
         console.log(worddata);
+
+       //Data gives many definitions but we only want 1. Eliminate xcess definitions using .split() and set definition state
+        const nounDefinition = worddata.meaning.noun.split('\n')[0].substring(6);
+        setNounDefinition(nounDefinition);
+        console.log("NDEF", nounDefinition)
+        
        
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -28,23 +36,24 @@ function CardMatches() {
     fetchWordData();
   }, []);
 
+
   return (
     <div>
     {data ? (
       <SimpleGrid cols={3}>
         {data.meaning && data.meaning.noun && (
           <>
-            <div>{data.meaning.noun}</div>
+            <div>{nounDefinition}</div>
             <div>2</div>
             <div>{data.entry}</div>
             <div>4</div>
-            <div>{data.entry}</div>
+            <div>6</div>
             <div>5</div>
-            <div>{data.entry}</div>
             <div>5</div>
-            <div>{data.entry}</div>
             <div>5</div>
-            <div>{data.entry}</div>
+            <div>5</div>
+            <div>5</div>
+            <div>2</div>
             <div>5</div>
           </>
         )}
