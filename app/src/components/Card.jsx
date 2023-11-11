a1bb79f36727f525e6e1ef41d280ac43edd6f293
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-11-11 12:55:23 -0500

    got the words and definitions to shuffle randomly and fill cards

--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,9 +1,52 @@
+// import { useState, useEffect } from "react";
+// import { SimpleGrid } from "@mantine/core";
 
+// const Card = () => {
+//   const [wordData, setWordData] = useState([]);
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+//   useEffect(() => {
+//     async function fetchWordData() {
+//       try {
+//         const wordArr = [];
+//         for (let i = 0; i < 6; i++) {
+//           const response = await fetch("http://localhost:80/giveword");
 
-export default Card
+//           if (!response.ok) {
+//             throw new Error(`Request failed with status: ${response.status}`);
+//           }
+
+//           const worddata = await response.json();
+//           // Ensure each word object has a nounDefinition property
+//           wordArr.push({
+//             entry: worddata.entry,
+//             nounDefinition: worddata.meaning.noun.split('\n')[0].substring(6)
+//           });
+//         }
+//         console.log(wordArr)
+//         setWordData(wordArr);
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//       }
+//     }
+
+//     fetchWordData();
+//   }, []);
+
+//   const stat = (
+//     <SimpleGrid cols={3}>
+//       {wordData.map((word, index) => (
+//         <div key={index}>
+//           <div>{word.entry}</div>
+//           <div>{word.nounDefinition}</div>
+//         </div>
+//       ))}
+//     </SimpleGrid>
+//   );
+
+//   return (
+//     <div className="container">
+//       {stat}
+//     </div>
+//   );
+// }
+// export default Card
--- app/src/components/CardMatches.jsx
+++ app/src/components/CardMatches.jsx
@@ -1,17 +1,30 @@
 import { useState, useEffect } from "react";
 import { SimpleGrid } from "@mantine/core";
-// import Card from "./Card";
+
+function shuffleArray(array) {
+  // Create a copy of the array
+  const shuffledArray = [...array];
+
+  // Function to shuffle the copied array using Fisher-Yates algorithm
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  console.log("SHUFFLEDARRAY", shuffledArray);
+  return shuffledArray;
+}
 
 function CardMatches() {
-  // const [data, setData] = useState([]);
   const [wordData, setWordData] = useState([]);
-  // const [nounDefinition, setNounDefinition] = useState("");
+  const [definitionData, setdefinitionData] = useState([]);
 
   useEffect(() => {
-
     async function fetchWordData() {
       try {
-        const wordArr = []
+        const wordArr = [];
+        const definitionArr = []
+
         for (let i = 0; i < 3; i++) {
           const response = await fetch("http://localhost:80/giveword");
 
@@ -19,62 +32,128 @@ function CardMatches() {
             throw new Error(`Request failed with status: ${response.status}`);
           }
 
-          const worddata = await response.json();
-          //Get response from API and set the state
-          wordArr.push(worddata);
-          console.log(worddata);
+          const apiData = await response.json();
+          // Ensure each word object has a nounDefinition property
+          wordArr.push({
+            entry: apiData.entry,
+          });
 
-          //Data gives many definitions but we only want 1. Eliminate xcess definitions using .split() and set definition state
-           const nounDefinition = worddata.meaning.noun.split('\n')[0].substring(6);
-           console.log("NDEF", nounDefinition)
-           wordArr[i].nounDefinition = nounDefinition;
+          definitionArr.push({
+            nounDefinition: apiData.meaning.noun.split("\n")[0].substring(6)
+          });
+          
         }
+        console.log("WORDARR", wordArr);
+        console.log("DEFARR", definitionArr);
+        // Shuffle the copied array before setting the state
+        setWordData(shuffleArray(wordArr));
+        setdefinitionData(shuffleArray(definitionArr))
 
-        setWordData(wordArr);
-       
+        console.log("SET", setWordData);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
-      
     }
-    
+
     fetchWordData();
-  }, []);
+  }, []); // Empty dependency array to run only once on mount
 
+  const stat = (
+  <SimpleGrid cols={3}>
+    {wordData.map((word, index) => (
+      <div key={index}>
+        <div>{word.entry}</div>
+        <div>{definitionData[index].nounDefinition}</div>
+      </div>
+    ))}
+  </SimpleGrid>
+);
 
-  const stat = 
-      <SimpleGrid cols={3}>
-        {wordData.map((word, index) => (
-          <div key={index}>
-            <div>{word.entry}</div>
-            <div>{word.nounDefinition}</div>
-  
-          </div>
-        ))}
-      </SimpleGrid>
-   
-
-  return (
-    <div className="container">
-      {/* <SimpleGrid cols={3}>
-          <>
-            <div>{nounDefinition}</div>
-            <div>2</div>
-            <div>{data.entry}</div>
-            <div>4</div>
-            <div>6</div>
-            <div>5</div>
-            <div>5</div>
-            <div>5</div>
-            <div>5</div>
-            <div>5</div>
-            <div>2</div>
-            <div>5</div>
-          </>
-      </SimpleGrid> */}
-      {stat}
-  </div>
-  );
+  return <div className="container">{stat}</div>;
 }
 
 export default CardMatches;
+
+
+// import { useState, useEffect } from "react";
+// import { SimpleGrid } from "@mantine/core";
+
+// function shuffleArray(array) {
+//   // Create a copy of the array
+//   const shuffledArray = [...array];
+
+//   // Function to shuffle the copied array using Fisher-Yates algorithm
+//   for (let i = shuffledArray.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+//   }
+
+//   return shuffledArray;
+// }
+
+// function CardMatches() {
+//   const [wordData, setWordData] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchWordData() {
+//       try {
+//         const words = [];
+//         const definitions = [];
+
+//         for (let i = 0; i < 2; i++) {
+//           const response = await fetch("http://localhost:80/giveword");
+
+//           if (!response.ok) {
+//             throw new Error(`Request failed with status: ${response.status}`);
+//           }
+
+//           const worddata = await response.json();
+//           // Ensure each word object has a nounDefinition property
+//           words.push(worddata.entry);
+//           definitions.push(worddata.meaning.noun.split('\n')[0].substring(6));
+//         }
+
+//         // Shuffle the arrays separately
+//         const shuffledWords = shuffleArray(words);
+//         const shuffledDefinitions = shuffleArray(definitions);
+
+//         // Combine shuffled words and definitions into pairs
+//         const wordPairs = shuffledWords.map((word, index) => ({
+//           entry: word,
+//           nounDefinition: shuffledDefinitions[index]
+//         }));
+
+//         // Set the state with the shuffled pairs
+//         setWordData(wordPairs);
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//       }
+//     }
+
+//     fetchWordData();
+//   }, []); // Empty dependency array to run only once on mount
+
+//   const stat = (
+//     <SimpleGrid cols={3}>
+//       {wordData.map((pair, index) => (
+//         <div key={index}>
+//           <div>{pair.entry}</div>
+//           <div>{pair.nounDefinition}</div>
+//         </div>
+//       ))}
+//     </SimpleGrid>
+//   );
+
+//   return (
+//     <div className="container">
+//       {stat}
+//     </div>
+//   );
+// }
+
+// export default CardMatches;
+
+
+
+
+
