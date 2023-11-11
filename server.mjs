5f2fa3dd71061ed4935a745855ca08e9a9245d7a
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-11-11 12:56:08 -0500

    server and env

--- server.mjs
+++ server.mjs
@@ -1,38 +1,71 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
-import words from './app/helpers/words.js'
+import words from './app/src/components/helpers/words.js'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
+const key = process.env.API_KEY
 
 app.use(cors());
 
+
 app.get("/giveword", async (req, res) => {
+
 
   const randomIndex = Math.floor(Math.random() * words.length);
   const randomWord = words[randomIndex];
 
-  const url = `https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=${randomWord}`;
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '171a179fffmshee0dd46533af305p1c57bbjsn26cb43cb20f8',
-      'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
-    }
-  };
-
   try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    console.log(result);
+    const response = await fetch(`https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=${randomWord}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        'X-RapidAPI-Key': `${key}`,
+        'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      res.json(data);
+    } else {
+
+      res.status(response.status).send('Error fetching data');
+    }
   } catch (error) {
-    console.error(error);
+    console.error("An error occurred:", error);
+    res.status(500).send('Internal Server Error');
   }
-
 });
-
 
 app.listen(port, () => {
   console.log(`Port is listening on port ${port}`)
 })
+
+
+// app.get("/bb", async (req, res) => {
+//   try {
+//     const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes', {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       }
+//     });
+
+//     if (response.ok) {
+//       const data = await response.json();
+//       console.log(data);
+//       res.json(data); // Send the JSON response to the client
+//     } else {
+//       // Handle non-OK response (e.g., 404, 500, etc.)
+//       res.status(response.status).send('Error fetching data');
+//     }
+//   } catch (error) {
+//     console.error("An error occurred:", error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
