a9df752c7ba0efd768e87e5a15972867008094e8
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-12-23 12:14:38 -0500

    vite config edits

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dns from 'dns'
+// import dns from 'dns'
 
 
-dns.setDefaultResultOrder('verbatim');
+// dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: 'localhost',
     port: 3000
+  }, build: {
+    outDir: 'dist'
   }
 })
