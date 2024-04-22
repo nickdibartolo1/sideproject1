7287e74214c563d5da7320dc459d9b99282a48f3
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-04-22 17:06:49 -0400

    changes

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,16 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import dns from 'dns'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// // import dns from 'dns'
 
 
-// dns.setDefaultResultOrder('verbatim');
+// // dns.setDefaultResultOrder('verbatim');
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: 'localhost',
+//     port: 3000
+//   }, build: {
+//     outDir: 'dist'
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: 'localhost',
     port: 3000
-  }, build: {
-    outDir: 'dist'
-  }
-})
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: ''
+      }
+    }
+  },
+  publicDir: 'public'
+});
