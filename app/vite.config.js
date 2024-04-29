9a6122eabfca19a4f140ff0bd2460dbcf80b508e
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-04-29 17:22:44 -0400

    changes in config3

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,34 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3000
-  }, build: {
-    outDir: 'dist'
-  }
-})
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // export default defineConfig({
-//   plugins: [
-//     react(),
-//   ],
+//   plugins: [react()],
 //   server: {
 //     host: 'localhost',
 //     port: 3000
-//   },
-//   build: {
-//     outDir: 'dist',
-//     rollupOptions: {
-//       input: {
-//         main: './src/main.jsx'
-//       }
-//     }
+//   }, build: {
+//     outDir: 'dist'
 //   }
-// });
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  server: {
+    host: 'localhost',
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        // main: './src/main.jsx'
+        main: '/index.html'
+      }
+    }
+  }
+});
 
